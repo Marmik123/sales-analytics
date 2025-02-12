11cf4cf0d731ed053241419cfd0fc61539c6ff2f
Marmik123 <marmikmodi209@gmail.com>
2025-02-13 01:47:13 +0530

    Added bullet points in select * statements

--- sales-analytics.sql
+++ sales-analytics.sql
@@ -149,7 +149,8 @@ TopLoyaltyCustomers AS (
     LIMIT 5
 )
 
---1
+--1 .Feedback Comparison (i.e. Average Feedback score of the same product when bought from different channels (Store type)).
+
 SELECT * FROM FeedbackComparison;
 -- OUTPUT:
 -- "productname"  "storetype"   "avgfeedbackscore" "totaltransactions"
@@ -160,14 +161,16 @@ SELECT * FROM FeedbackComparison;
 -- "Notebook"     "Online"      3.009              45251
 -- "Notebook"     "In-Store"    2.996              45043
 
---2
+--2 Product with Highest Return rate 
+
 SELECT * FROM ProductReturnRate;
 -- OUTPUT:
 -- "productname"  "totalorders"  "returnedorders"  "returnrate"
 -- "Apple"        89970         45033             50.05
 
 
---3
+--3 Most Popular Products by Sales Volume
+
 SELECT * FROM PopularProducts;
 -- OUTPUT:
 -- "productname"  "totalquantitysold"  "totalrevenue"
@@ -177,7 +180,8 @@ SELECT * FROM PopularProducts;
 -- "Laptop"      89809                 6231220430.24
 -- "Sofa"        89740                 3777022903.56
 
---4
+--4 Top 3 preferred mode of payment on the basis of orders count.
+
 SELECT * FROM TopPaymentMethods;
 -- OUTPUT:
 -- "paymentmethod"  "totalorders"
@@ -185,7 +189,8 @@ SELECT * FROM TopPaymentMethods;
 -- "Cash"           112625
 -- "UPI"            112517
 
---5
+--5 Top 3 Cities in terms of orders count.
+
 SELECT * FROM TopCities;
 -- OUTPUT:
 -- "city"         "totalorders"
@@ -193,13 +198,14 @@ SELECT * FROM TopCities;
 -- "Delhi"        50215
 -- "Lucknow"      50190
 
---6
+--6 Top customer in terms of orders count.
 SELECT * FROM TopCustomer;
 -- OUTPUT:
 -- "customerid"  "totalorders"
 -- 39402        24
 
---7
+--7 Orders count by Age Range
+
 SELECT * FROM OrdersByAge;
 -- OUTPUT:
 -- "agerange"  "totalorders"
@@ -209,7 +215,7 @@ SELECT * FROM OrdersByAge;
 -- "26-35"     79001
 -- "18-25"     62947
 
---8
+--8 orders count by gender.
 SELECT * FROM OrdersByGender;
 -- OUTPUT:
 -- "customergender"  "totalorders"
@@ -217,7 +223,7 @@ SELECT * FROM OrdersByGender;
 -- "Male"          149970
 -- "Female"        149773
 
---9
+--9 Top 3 regions in terms of orders.
 SELECT * FROM TopRegions;
 -- OUTPUT:
 -- "region"  "totalorders"
@@ -225,7 +231,7 @@ SELECT * FROM TopRegions;
 -- "East"    118910
 -- "West"    96167
 
---10
+--10 Sales Breakdown by Region
 SELECT * FROM SalesByRegion;
 -- OUTPUT:
 -- "region"  "totalrevenue"     "totaltransactions"
@@ -234,14 +240,15 @@ SELECT * FROM SalesByRegion;
 -- "North"   2171502697.87      96166
 -- "West"    2159911845.88      96167
 
---11
+--11 Store type performance
+
 SELECT * FROM StorePerformance;
 -- OUTPUT:
 -- "storetype"  "revenue"
 -- "In-Store"   5078881502.74
 -- "Online"     5078048214.51
 
---12
+--12  Return Rate by Product
 SELECT * FROM ProductReturnCounts;
 -- OUTPUT:
 -- "productname"  "returncount"
@@ -251,7 +258,7 @@ SELECT * FROM ProductReturnCounts;
 -- "T-Shirt"      44783
 -- "Sofa"         44696
 
---13
+--13 Top Loyalty Point Customers
 SELECT * FROM TopLoyaltyCustomers;
 -- OUTPUT:
 -- "customerid"  "totalpoints"
