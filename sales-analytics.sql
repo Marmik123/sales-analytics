a8132c065fa5f14bf957d40c5f5a3e64dc8962b4
Marmik123 <marmikmodi209@gmail.com>
2025-02-13 01:17:24 +0530

    6. top customers in terms of orders count and 7. orders count by age range

--- sales-analytics.sql
+++ sales-analytics.sql
@@ -32,7 +32,8 @@ FROM sales_data
 GROUP BY ProductName, StoreType
 HAVING productname is not null and storetype is not null
 ORDER BY ProductName, AvgFeedbackScore DESC;
---OUTPUT
+
+--OUTPUT 1
 "productname"	"storetype"	"avgfeedbackscore"	"totaltransactions"
 "Apple"	        "In-Store"	    3.006	        44793
 "Apple"	        "Online"	    3.001	        45177
@@ -77,7 +78,7 @@ LIMIT 10;
 "Laptop"	         89809	          6231220430.24
 "Sofa"	             89740	          3777022903.56
 
---4 Top 3 preferred mode of payment.
+--4 Top 3 preferred mode of payment on the basis of orders count.
 SELECT PaymentMethod, COUNT(*) AS TotalOrders
 FROM sales_data
 GROUP BY PaymentMethod
@@ -103,3 +104,40 @@ LIMIT 1;
 "Delhi"	        50215
 "Lucknow"	    50190
 
+
+--6 Top customer in terms of orders count.
+SELECT CustomerID, COUNT(*) AS TotalOrders
+FROM sales_data
+GROUP BY CustomerID
+HAVING customerid is not null
+ORDER BY TotalOrders DESC
+LIMIT 1;
+
+--OUTPUT 6
+"customerid"	"totalorders"
+39402	            24
+
+
+--7 Orders count by Age Range
+SELECT 
+    CASE 
+        WHEN CustomerAge BETWEEN 18 AND 25 THEN '18-25'
+        WHEN CustomerAge BETWEEN 26 AND 35 THEN '26-35'
+        WHEN CustomerAge BETWEEN 36 AND 45 THEN '36-45'
+        WHEN CustomerAge BETWEEN 46 AND 60 THEN '46-60'
+		WHEN CustomerAge IS NULL THEN '0'
+        ELSE '60+'
+    END AS AgeRange,
+    COUNT(*) AS TotalOrders
+FROM sales_data
+GROUP BY AgeRange
+ORDER BY TotalOrders DESC
+LIMIT 5;
+
+--OUTPUT 7
+"agerange"	"totalorders"
+"46-60"	        118833
+"60+"	        110116
+"36-45"	        79103
+"26-35"	        79001
+"18-25"     	62947
