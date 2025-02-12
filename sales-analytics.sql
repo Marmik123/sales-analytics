22fa6aeb3f4f8bde008592729504e47f5d08bc94
Marmik123 <marmikmodi209@gmail.com>
2025-02-13 01:40:22 +0530

    Made it structured, modular, and reusable using Common table expressions (CTE)

--- sales-analytics.sql
+++ sales-analytics.sql
@@ -22,207 +22,227 @@ CREATE TABLE sales_data (
 );
 
 
---1.Feedback Comparison (i.e. Average Feedback score of the same product when bought from different channels (Store type)).
-SELECT 
-    ProductName, 
-    StoreType, 
-    ROUND(AVG(FeedbackScore),3) AS AvgFeedbackScore,
-    COUNT(*) AS TotalTransactions
-FROM sales_data
-GROUP BY ProductName, StoreType
-HAVING productname is not null and storetype is not null
-ORDER BY ProductName, AvgFeedbackScore DESC;
+WITH FeedbackComparison AS (
+    SELECT 
+        ProductName, StoreType, 
+        ROUND(AVG(FeedbackScore),3) AS AvgFeedbackScore,
+        COUNT(*) AS TotalTransactions
+    FROM sales_data
+    WHERE ProductName IS NOT NULL AND StoreType IS NOT NULL
+    GROUP BY ProductName, StoreType
+), 
 
---OUTPUT 1
-"productname"	"storetype"	"avgfeedbackscore"	"totaltransactions"
-"Apple"	        "In-Store"	    3.006	        44793
-"Apple"	        "Online"	    3.001	        45177
-"Laptop"	    "In-Store"	    3.003	        44867
-"Laptop"	    "Online"	    2.990	        44942
-"Notebook"	    "Online"	    3.009	        45251
-"Notebook"	    "In-Store"	    2.996	        45043
+ProductReturnRate AS (
+    SELECT 
+        ProductName, 
+        COUNT(*) AS TotalOrders,
+        SUM(CASE WHEN Returned = 'Yes' THEN 1 ELSE 0 END) AS ReturnedOrders,
+        ROUND((SUM(CASE WHEN Returned = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*)),2)  AS ReturnRate
+    FROM sales_data
+    GROUP BY ProductName
+    ORDER BY ReturnRate DESC
+    LIMIT 1
+), 
 
+PopularProducts AS (
+    SELECT 
+        ProductName, 
+        SUM(Quantity) AS TotalQuantitySold,
+        SUM(TransactionAmount) AS TotalRevenue
+    FROM sales_data
+    WHERE ProductName IS NOT NULL
+    GROUP BY ProductName
+    ORDER BY TotalQuantitySold DESC
+    LIMIT 10
+), 
 
+TopPaymentMethods AS (
+    SELECT PaymentMethod, COUNT(*) AS TotalOrders
+    FROM sales_data
+    GROUP BY PaymentMethod
+    ORDER BY TotalOrders DESC
+    LIMIT 3
+), 
 
--- 2. Product with Highest Return rate 
-SELECT 
-    ProductName, 
-    COUNT(*) AS TotalOrders,
-    SUM(CASE WHEN Returned = 'Yes' THEN 1 ELSE 0 END) AS ReturnedOrders,
-    ROUND((SUM(CASE WHEN Returned = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*)),2)  AS ReturnRate
-FROM sales_data
-GROUP BY ProductName
-ORDER BY returnrate DESC
-LIMIT 1;
---OUTPUT 2
-"productname"	"totalorders"	"returnedorders"	"returnrate"
-   "Apple"	        89970	        45033	            50.05
+TopCities AS (
+    SELECT City, COUNT(*) AS TotalOrders
+    FROM sales_data
+    GROUP BY City
+    ORDER BY TotalOrders DESC
+    LIMIT 3
+), 
 
+TopCustomer AS (
+    SELECT CustomerID, COUNT(*) AS TotalOrders
+    FROM sales_data
+    WHERE CustomerID IS NOT NULL
+    GROUP BY CustomerID
+    ORDER BY TotalOrders DESC
+    LIMIT 1
+), 
 
---3  Most Popular Products by Sales Volume
-SELECT 
-    ProductName, 
-    SUM(Quantity) AS TotalQuantitySold,
-    SUM(TransactionAmount) AS TotalRevenue
-FROM sales_data
-GROUP BY ProductName
-HAVING productname is not null
-ORDER BY TotalQuantitySold DESC
-LIMIT 10;   
+OrdersByAge AS (
+    SELECT 
+        CASE 
+            WHEN CustomerAge BETWEEN 18 AND 25 THEN '18-25'
+            WHEN CustomerAge BETWEEN 26 AND 35 THEN '26-35'
+            WHEN CustomerAge BETWEEN 36 AND 45 THEN '36-45'
+            WHEN CustomerAge BETWEEN 46 AND 60 THEN '46-60'
+            WHEN CustomerAge IS NULL THEN '0'
+            ELSE '60+'
+        END AS AgeRange,
+        COUNT(*) AS TotalOrders
+    FROM sales_data
+    GROUP BY AgeRange
+    ORDER BY TotalOrders DESC
+    LIMIT 5
+), 
 
---OUTPUT 3
-"productname"	"totalquantitysold"	"totalrevenue"
-"Apple"	             2296713	      22300717.86
-"Notebook"	         498649	          24079586.12
-"T-Shirt"	         270545	          102306079.47
-"Laptop"	         89809	          6231220430.24
-"Sofa"	             89740	          3777022903.56
+OrdersByGender AS (
+    SELECT CustomerGender, COUNT(*) AS TotalOrders
+    FROM sales_data
+    GROUP BY CustomerGender
+    ORDER BY TotalOrders DESC
+    LIMIT 1
+), 
 
---4 Top 3 preferred mode of payment on the basis of orders count.
-SELECT PaymentMethod, COUNT(*) AS TotalOrders
-FROM sales_data
-GROUP BY PaymentMethod
-ORDER BY TotalOrders DESC
-LIMIT 3;
+TopRegions AS (
+    SELECT Region, COUNT(*) AS TotalOrders
+    FROM sales_data
+    GROUP BY Region
+    ORDER BY TotalOrders DESC
+    LIMIT 3
+), 
 
---OUTPUT 4
-"paymentmethod"	"totalorders"
-"Debit Card"	   113015
-"Cash"	           112625
-"UPI"	           112517
+SalesByRegion AS (
+    SELECT 
+        Region, 
+        SUM(TransactionAmount) AS TotalRevenue, 
+        COUNT(*) AS TotalTransactions
+    FROM sales_data
+    WHERE Region IS NOT NULL
+    GROUP BY Region
+    ORDER BY TotalRevenue DESC
+), 
 
---5 Top 3 Cities in terms of orders count.
-SELECT City, COUNT(*) AS TotalOrders
-FROM sales_data
-GROUP BY City
-ORDER BY TotalOrders DESC
-LIMIT 1;
+StorePerformance AS (
+    SELECT StoreType, SUM(TransactionAmount) AS Revenue
+    FROM sales_data
+    WHERE StoreType IS NOT NULL
+    GROUP BY StoreType
+    ORDER BY Revenue DESC
+), 
 
---OUTPUT 5
-"city"	    "totalorders"
-"Bangalore"	    50319
-"Delhi"	        50215
-"Lucknow"	    50190
+ProductReturnCounts AS (
+    SELECT ProductName, COUNT(*) AS ReturnCount
+    FROM sales_data
+    WHERE Returned = 'Yes'
+    GROUP BY ProductName
+    ORDER BY ReturnCount DESC
+), 
 
+TopLoyaltyCustomers AS (
+    SELECT CustomerID, SUM(LoyaltyPoints) AS TotalPoints
+    FROM sales_data
+    GROUP BY CustomerID
+    ORDER BY TotalPoints DESC
+    LIMIT 5
+)
 
---6 Top customer in terms of orders count.
-SELECT CustomerID, COUNT(*) AS TotalOrders
-FROM sales_data
-GROUP BY CustomerID
-HAVING customerid is not null
-ORDER BY TotalOrders DESC
-LIMIT 1;
+SELECT * FROM FeedbackComparison;
+-- OUTPUT:
+-- "productname"  "storetype"   "avgfeedbackscore" "totaltransactions"
+-- "Apple"        "In-Store"    3.006              44793
+-- "Apple"        "Online"      3.001              45177
+-- "Laptop"       "In-Store"    3.003              44867
+-- "Laptop"       "Online"      2.990              44942
+-- "Notebook"     "Online"      3.009              45251
+-- "Notebook"     "In-Store"    2.996              45043
 
---OUTPUT 6
-"customerid"	"totalorders"
-39402	            24
+SELECT * FROM ProductReturnRate;
+-- OUTPUT:
+-- "productname"  "totalorders"  "returnedorders"  "returnrate"
+-- "Apple"        89970         45033             50.05
 
+SELECT * FROM PopularProducts;
+-- OUTPUT:
+-- "productname"  "totalquantitysold"  "totalrevenue"
+-- "Apple"       2296713               22300717.86
+-- "Notebook"    498649                 24079586.12
+-- "T-Shirt"     270545                102306079.47
+-- "Laptop"      89809                 6231220430.24
+-- "Sofa"        89740                 3777022903.56
 
---7 Orders count by Age Range
-SELECT 
-    CASE 
-        WHEN CustomerAge BETWEEN 18 AND 25 THEN '18-25'
-        WHEN CustomerAge BETWEEN 26 AND 35 THEN '26-35'
-        WHEN CustomerAge BETWEEN 36 AND 45 THEN '36-45'
-        WHEN CustomerAge BETWEEN 46 AND 60 THEN '46-60'
-		WHEN CustomerAge IS NULL THEN '0'
-        ELSE '60+'
-    END AS AgeRange,
-    COUNT(*) AS TotalOrders
-FROM sales_data
-GROUP BY AgeRange
-ORDER BY TotalOrders DESC
-LIMIT 5;
+SELECT * FROM TopPaymentMethods;
+-- OUTPUT:
+-- "paymentmethod"  "totalorders"
+-- "Debit Card"     113015
+-- "Cash"           112625
+-- "UPI"            112517
 
---OUTPUT 7
-"agerange"	"totalorders"
-"46-60"	        118833
-"60+"	        110116
-"36-45"	        79103
-"26-35"	        79001
-"18-25"     	62947
+SELECT * FROM TopCities;
+-- OUTPUT:
+-- "city"         "totalorders"
+-- "Bangalore"    50319
+-- "Delhi"        50215
+-- "Lucknow"      50190
 
---8 orders count by gender.
-SELECT CustomerGender, COUNT(*) AS TotalOrders
-FROM sales_data
-GROUP BY CustomerGender
-ORDER BY TotalOrders DESC
-LIMIT 1;
+SELECT * FROM TopCustomer;
+-- OUTPUT:
+-- "customerid"  "totalorders"
+-- 39402        24
 
---OUTPUT 8
-"customergender"	"totalorders"
-"Other"	            150257
-"Male"	            149970
-"Female"	        149773
+SELECT * FROM OrdersByAge;
+-- OUTPUT:
+-- "agerange"  "totalorders"
+-- "46-60"     118833
+-- "60+"       110116
+-- "36-45"     79103
+-- "26-35"     79001
+-- "18-25"     62947
 
---9 Top 3 regions in terms of orders.
-SELECT Region, COUNT(*) AS TotalOrders
-FROM sales_data
-GROUP BY Region
-ORDER BY TotalOrders DESC
-LIMIT 3;
+SELECT * FROM OrdersByGender;
+-- OUTPUT:
+-- "customergender"  "totalorders"
+-- "Other"          150257
+-- "Male"          149970
+-- "Female"        149773
 
---OUTPUT 9
-"region"	"totalorders"
-"South"	       146124
-"East"	       118910
-"West"	       96167
+SELECT * FROM TopRegions;
+-- OUTPUT:
+-- "region"  "totalorders"
+-- "South"   146124
+-- "East"    118910
+-- "West"    96167
 
---10 Sales Breakdown by Region
-SELECT 
-    Region, 
-    SUM(TransactionAmount) AS TotalRevenue, 
-    COUNT(*) AS TotalTransactions
-FROM sales_data
-GROUP BY Region
-HAVING REGION IS NOT NULL
-ORDER BY TotalRevenue DESC;
+SELECT * FROM SalesByRegion;
+-- OUTPUT:
+-- "region"  "totalrevenue"     "totaltransactions"
+-- "South"   3177273109.38      146124
+-- "East"    2654969082.59      118910
+-- "North"   2171502697.87      96166
+-- "West"    2159911845.88      96167
 
---OUTPUT 10
-"region"	"totalrevenue"	"totaltransactions"
-"South"	3177273109.38	    146124
-"East"	2654969082.59	    118910
-"North"	2171502697.87	    96166
-"West"	2159911845.88	    96167
+SELECT * FROM StorePerformance;
+-- OUTPUT:
+-- "storetype"  "revenue"
+-- "In-Store"   5078881502.74
+-- "Online"     5078048214.51
 
---11 Store type performance
-SELECT StoreType, SUM(TransactionAmount) AS Revenue
-FROM sales_data
-GROUP BY StoreType
-HAVING storetype is not null
-ORDER BY Revenue DESC;
+SELECT * FROM ProductReturnCounts;
+-- OUTPUT:
+-- "productname"  "returncount"
+-- "Notebook"     45061
+-- "Apple"        45033
+-- "Laptop"       44904
+-- "T-Shirt"      44783
+-- "Sofa"         44696
 
---OUTPUT 11
-"storetype"	"revenue"
-"In-Store"	5078881502.74
-"Online"	5078048214.51
-	
---12 Return Rate by Product
-SELECT ProductName, COUNT(*) AS ReturnCount
-FROM sales_data
-WHERE Returned = TRUE
-GROUP BY ProductName
-ORDER BY ReturnCount DESC;
-
---OUTPUT 12
-"productname"	"returncount"
-"Notebook"	    45061
-"Apple"	        45033
-"Laptop"	    44904
-"T-Shirt"	    44783
-"Sofa"	        44696    
-
---13 Top Loyalty Point Customers
-SELECT CustomerID, SUM(LoyaltyPoints) AS TotalPoints
-FROM sales_data
-GROUP BY CustomerID
-ORDER BY TotalPoints DESC
-LIMIT 5;
-
---OUTPUT 13
-"customerid"	"totalpoints"
-13497	        132369
-15834	        131318
-39402	        130699
-24925	        128162
-44185	        123424
+SELECT * FROM TopLoyaltyCustomers;
+-- OUTPUT:
+-- "customerid"  "totalpoints"
+-- 13497         132369
+-- 15834         131318
+-- 39402         130699
+-- 24925         128162
+-- 44185         123424
